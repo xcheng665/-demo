76647b797c882994afd354500746dcc2c1b76de3
xcheng665 <18879819661a@gmail.com>
2026-08-21 10:06:50 +0800

    Fix mobile homepage layout spacing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./project-cover-final.css";
+import "./home-mobile-layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
